d834e68132841a860c8ae5a7ba1593a55062ef35
T-and0 <cs14003@s.inf.shizuoka.ac.jp>
2016-01-31 12:40:02 +0900

    Update setting.h

--- setting.h
+++ setting.h
@@ -1,6 +1,14 @@
-#define ROW 12
-#define COL 13
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+#define MAXSIZE 100
+/*棚の最大番号*/
+#define SHELF_MAX 500
+#define  INIT_MAX 999999
+/*メモの最大メニュー数*/
+#define MENUSIZE 100
+#define SHOPITEMSIZE 130
 typedef struct{
   int r;
   int c;
