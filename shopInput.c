0da70e6d220fa3ae726050316a92cba288e52244
T-and0 <cs14003@s.inf.shizuoka.ac.jp>
2016-02-05 23:29:45 +0900

    Update shopInput.c

--- shopInput.c
+++ shopInput.c
@@ -8,7 +8,10 @@
 extern int map[MAXSIZE][MAXSIZE];
 extern int ROW;
 extern int COL;
-
+char *GetShopName();
+int selected_shop;
+char shop[100][MAXCHARS];
+char figure[100][MAXCHARS];
 int shopInput(){
   char filename[MAXCHARSFILENAME],line[MAXCHARS];
   int i,j;
@@ -17,8 +20,7 @@ int shopInput(){
   FILE *fp;
   char *ret;
   int shop_cnt;
-  char shop[100][MAXCHARS];
-  char figure[100][MAXCHARS];
+  
 
   shop_cnt=0;
   /*店リスト取得*/
@@ -38,38 +40,49 @@ int shopInput(){
        shop_cnt++;
     }
    }else{
-     fprintf(stderr,"shoplist.txtがありません\n");  
-      return -1;
-    }
+     fprintf(stderr,"shoplist.txtがありません\n");
+fprintf(stderr,"(例)\n店1:店1の構造図を記述したファイル名\n店2:店2の構造図を記述したファイル名\n");  
+     return -1;
+   }
    fclose(fp);
  if(shop_cnt==0){
-    fprintf(stderr,"店情報がありません\n");
+    fprintf(stderr,"店情報が登録されていません\n");
   }
  for(i=0;i<shop_cnt;i++){
     fprintf(stderr,"(%d)%s : %s\n",i,shop[i],figure[i]);
   }
 
   /*行く店を決める*/
- int select = -1;
+ selected_shop=-1;
  char buf[10];
  
- while(select ==-1){
+ while(selected_shop ==-1){
+fprintf(stderr,"shop番号(0~%d)を入力してください \n",shop_cnt-1);
    fgets( buf, 10, stdin );
-   if(buf[0]!='\n'){
-     select = atoi(buf);
+
+
+    for(i=0;i<10;i++){
+       if(buf[i]==10)
+         break;
+       if(buf[i]<'0'||buf[i]>'9')
+          buf[0]=10;
+   }
+
+   
+   if(buf[0]!=10){
+     selected_shop = atoi(buf);
    }else{
-     fprintf(stderr,"shop番号(0~%d)を入力してください \n",shop_cnt-1);
      continue;
    }
  
-   if(select >= shop_cnt){
+   if(selected_shop >= shop_cnt){
      fprintf(stderr,"shopは 0 ~ %d です\n",shop_cnt-1);
-       select = -1;
+       selected_shop = -1;
    }
  }
- fprintf(stderr,"%s を選択しました\n",shop[select]);
+ fprintf(stderr,"%s を選択しました\n",shop[selected_shop]);
  
- strcpy(filename,figure[select]);
+ strcpy(filename,figure[selected_shop]);
   /* strcat(temp,filename);*/
   
   fp=(FILE*)fopen(filename,"r");
@@ -113,7 +126,7 @@ int shopInput(){
     fclose(fp);
     
   }else{
-    fprintf(stderr,"FILE NOT FOUND.\n");
+    fprintf(stderr,"指定された店の構造図 '%s' NOT FOUND.\n",figure[selected_shop]);
     return -1;
   }
 
@@ -129,6 +142,10 @@ int shopInput(){
     fprintf(stderr,"\n");
   }
 
-  fprintf(stderr,"店構造図を読み込みしました。\n");
+  fprintf(stderr,"店構造図を読込ました。\n");
   return 0;
+}
+
+char *GetShopName(){
+  return &(shop[selected_shop][0]);
 }
